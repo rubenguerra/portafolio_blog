4538bb919e190d0b4857a1bddc18c978c8c92426
rubenguerra <chopiterguerra@gmail.com>
2023-04-18 13:25:09 -0300

    Merge remote-tracking branch 'github/main'
    # Conflicts:
    #	blog_portafolio/blog/static/css/estilo.css
    #	blog_portafolio/blog/templates/blog/post.html
    #	blog_portafolio/blog_portafolio/settings.py

--- blog_portafolio/blog_portafolio/settings.py
+++ blog_portafolio/blog_portafolio/settings.py
@@ -94,10 +94,10 @@ WSGI_APPLICATION = 'blog_portafolio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'blog_portafolio',
-        'USER': 'root',
-        'PASSWORD': '9981143',
-        'HOST': '127.0.0.1',
+        'NAME': 'blog_backend',
+        'USER': 'admin_blog',
+        'PASSWORD': 'ruben9981143',
+        'HOST': 'blogbackend.cfk8cqorouzp.sa-east-1.rds.amazonaws.com',
         'PORT': '3306',
     }
 }
@@ -227,3 +227,14 @@ CKEDITOR_CONFIGS = {
 }
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
+
+AWS_ACCESS_KEY_ID = 'AKIAZVIRZUBHHSK6S753'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'blogbackend'
+AWS_S3_SIGNATURE_NAME = 's3v4'
+AWS_S3_REGION_NAME = 'sa-east-1'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_VERITY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
