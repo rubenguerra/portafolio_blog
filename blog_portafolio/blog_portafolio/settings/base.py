55cab868835c1399ffedde5734484271bf084dcf
rubenguerra <chopiterguerra@gmail.com>
2023-04-24 11:14:41 -0300

    Configurando settings para producción

--- blog_portafolio/blog_portafolio/settings/base.py
+++ blog_portafolio/blog_portafolio/settings/base.py
@@ -30,7 +30,6 @@ EMAIL_PORT = 587  # Puerto que se va a utilizar
 SECRET_KEY = 'django-insecure-_m_7ucjkb#-&&6ymb(^0dlt$6b!2cz!yo60=gum0v9dntspqxx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -91,16 +90,6 @@ WSGI_APPLICATION = 'blog_portafolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'blog_backend',
-        'USER': 'admin_blog',
-        'PASSWORD': 'ruben9981143',
-        'HOST': 'blogbackend.cfk8cqorouzp.sa-east-1.rds.amazonaws.com',
-        'PORT': '3306',
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -145,11 +134,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/mediafiles/'
 MEDIA_ROOT = BASE_DIR / 'mediafiles/'
 
-"""
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-
-"""
 
 # CKEDITOR_FILENAME_GENERATOR = 'utils.get_filename'
 
@@ -227,14 +211,3 @@ CKEDITOR_CONFIGS = {
 }
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
-
-
-AWS_ACCESS_KEY_ID = 'AKIAZVIRZUBHHSK6S753'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'blogbackend'
-AWS_S3_SIGNATURE_NAME = 's3v4'
-AWS_S3_REGION_NAME = 'sa-east-1'
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-AWS_S3_VERITY = True
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
---
+++ blog_portafolio/blog_portafolio/settings/local.py
@@ -0,0 +1,14 @@
+from .base import *
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog_portafolio',
+        'USER': 'root',
+        'PASSWORD': '9981143',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
